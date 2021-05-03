4f3b6302f164f00c18b2530c04a98f36904a0611
LETHAL-ARMY <83568428+LETHAL-ARMY@users.noreply.github.com>
2021-05-03 20:30:19 +0530

    | LETHAL |

--- userbot/plugins/extdl.py
+++ userbot/plugins/extdl.py
@@ -1,28 +1,6 @@
-#   Copyright 2019 - 2020 DarkPrinc3
-
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-
-
-
-# Thanks to Dark prince for this plugin..which was already here in DC but we were not awake of this..
-
-
-""" 
-   Thats the reason I say I am an idiot... To use this you have to fill the var PLUGIN_CHANNEL from which you want all your plugins to be installed.. Similar like installall.py / load_all.py which initially javes made..but it did existed here 
-Since a long time...
-
-"""
+# GNU LICENSED APPLICATION
+# ALL COPYRIGHTS RESERVED BY LETHAL CO-ORPERATION (C) 2021
+# SAFE APPLICATION CERTIFIED BY G.N.U ORGANISATION (c) 2021
 
 from userbot import bot
 from telethon import events
