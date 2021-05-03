22126a96525f8a198edf8763b267aca20fbcbd84
LETHAL-ARMY <83568428+LETHAL-ARMY@users.noreply.github.com>
2021-05-03 22:32:08 +0530

    | LETHAL |

--- userbot/plugins/information.py
+++ userbot/plugins/information.py
@@ -1,3 +1,6 @@
+# PROJECT LETHAL (c) 2021
+# GNU LICENSED APPLICATION
+# ALL RIGHTS RESERVED BY LETHAL CO-ORPERATION (c) 2021
 """Get Telegram Profile Picture and other information
 Syntax: .info @username"""
 
@@ -6,10 +9,11 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd
 
 
 @borg.on(admin_cmd("info ?(.*)"))
+@borg.on(sudo_cmd("info ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
