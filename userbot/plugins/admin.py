78bdbb4a181fca13cbbd04abf171fe66d3be0861
LETHAL-ARMY <83568428+LETHAL-ARMY@users.noreply.github.com>
2021-05-03 18:15:58 +0530

    | LETHAL |

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -23,7 +23,7 @@ from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
 
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot 
 from userbot.utils import register, errors_handler
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, SUDO_CMD
 
 # =================== CONSTANT ===================
 PP_TOO_SMOL = "`The image is too small`"
