6e0481989c5ceb66da918e95a0376ca2cd68dda6
LETHAL-ARMY <83568428+LETHAL-ARMY@users.noreply.github.com>
2021-05-04 10:08:34 +0530

    Update ppg.py

--- userbot/plugins/ppg.py
+++ userbot/plugins/ppg.py
@@ -6,10 +6,11 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, sudo_cmd
 
 
 @borg.on(admin_cmd(pattern="ppg ?(.*)"))
+@borg.on(sudo_cmd(pattern="ppg ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
