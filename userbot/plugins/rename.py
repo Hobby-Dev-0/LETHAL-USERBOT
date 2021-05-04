29fc42d865920dcfd3400034d36728a3d26095d0
LETHAL-ARMY <83568428+LETHAL-ARMY@users.noreply.github.com>
2021-05-04 12:08:56 +0530

    Update rename.py

--- userbot/plugins/rename.py
+++ userbot/plugins/rename.py
@@ -10,6 +10,7 @@ thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "thumb_image.jpg"
 
 
 @borg.on(admin_cmd(pattern="rename (.*)"))
+@borg.on(sudo_cmd(pattern="rename (.*)"))
 async def _(event):
     if event.fwd_from:
         return
